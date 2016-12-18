701abf559e2b65ce96026b6c6b66976be56783b1
steffenSuess <steffen.suess@hs-ansbach.de>
2016-12-18 15:29:59 +0100

    Added Barcode Functionality

--- app/src/main/java/com/example/steffensuess/price48/MainActivity.java
+++ app/src/main/java/com/example/steffensuess/price48/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.steffensuess.price48;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView barcodeResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String test = "Hallo";
+
+        barcodeResult = (TextView)findViewById(R.id.barcode_result);
+    }
+
+    public void scanBarcode(View view){
+        Intent intent = new Intent(this, ScanBarcodeActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==0){
+            if(resultCode== CommonStatusCodes.SUCCESS){
+                if(data!=null){
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    barcodeResult.setText("Barcode value: " + barcode.displayValue);
+                }else{
+                    barcodeResult.setText("No barcode found!");
+                }
+            }
+
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
